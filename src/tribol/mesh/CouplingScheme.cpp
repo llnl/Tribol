f57984a897c634deb86d042825df373de5734916
Jennifer E Fromm <fromm3@llnl.gov>
2025-03-19 11:45:35 -0700

    Update CouplingScheme.cpp

--- src/tribol/mesh/CouplingScheme.cpp
+++ src/tribol/mesh/CouplingScheme.cpp
@@ -1287,8 +1287,8 @@ void CouplingScheme::computeCommonPlaneTimeStep( RealT& dt )
   ArrayT<RealT> dt_temp_data( { dt, dt }, getAllocatorId() );
   ArrayViewT<RealT> dt_temp = dt_temp_data;
   // [0]: exceed_max_gap1, [1]: exceed_max_gap2, [2]: neg_dt_gap_msg, [3]: neg_dt_vel_proj_msg
-  ArrayT<bool> msg_data( { false, false, false, false }, getAllocatorId() );
-  ArrayViewT<bool> msg = msg_data;
+  ArrayT<IndexT> msg_data( { static_cast<IndexT>( false ), static_cast<IndexT>( false ), static_cast<IndexT>( false ), static_cast<IndexT>( false ) }, getAllocatorId() );
+  ArrayViewT<IndexT> msg = msg_data;
   forAllExec( getExecutionMode(), getNumActivePairs(),
               [cs_view, dim, proj_ratio, msg, dt_temp, dt] TRIBOL_HOST_DEVICE( IndexT i ) {
                 auto& plane = cs_view.getContactPlane( i );
@@ -1465,8 +1465,8 @@ void CouplingScheme::computeCommonPlaneTimeStep( RealT& dt )
                   dt2_check1 = ( dt2_vel_check ) ? exceed_max_gap2 : false;
 
 #ifdef TRIBOL_USE_RAJA
-                  RAJA::atomicExchange<RAJA::auto_atomic>( &msg[0], exceed_max_gap1 );
-                  RAJA::atomicExchange<RAJA::auto_atomic>( &msg[1], exceed_max_gap2 );
+                  RAJA::atomicMax<RAJA::auto_atomic>( &msg[0],  static_cast<IndexT>( exceed_max_gap1 ) );
+                  RAJA::atomicMax<RAJA::auto_atomic>( &msg[1],  static_cast<IndexT>( exceed_max_gap2 ) );
 #else
                   msg[0] = exceed_max_gap1;
                   msg[1] = exceed_max_gap2;
@@ -1513,9 +1513,12 @@ void CouplingScheme::computeCommonPlaneTimeStep( RealT& dt )
                   }
 
                   if ( dt1 < 0. || dt2 < 0. ) {
+#ifdef TRIBOL_USE_RAJA
+                    RAJA::atomicMax<RAJA::auto_atomic>( &msg[2], static_cast<IndexT>( true ) );
+#else
                     msg[2] = true;
+#endif
                   }
-
                 }  // end case 1
 
                 ////////////////////////////////////////////////////////////////////////
@@ -1629,7 +1632,11 @@ void CouplingScheme::computeCommonPlaneTimeStep( RealT& dt )
 #endif
                   }
                   if ( dt1 < 0. || dt2 < 0. ) {
+#ifdef TRIBOL_USE_RAJA
+                    RAJA::atomicMax<RAJA::auto_atomic>( &msg[3], static_cast<IndexT>( true ) );
+#else
                     msg[3] = true;
+#endif
                   }
 
                 }  // end check 2
