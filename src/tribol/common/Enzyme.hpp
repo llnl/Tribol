a05c50541f979bee7ca8639f515f60e8e8292285
Eric B. Chin <chin23@llnl.gov>
2025-03-27 22:24:52 -0700

    formtting and include

--- src/tribol/common/Enzyme.hpp
+++ src/tribol/common/Enzyme.hpp
@@ -6,13 +6,14 @@
 #ifndef TRIBOL_COMMON_ENZYME_HPP_
 #define TRIBOL_COMMON_ENZYME_HPP_
 
+// Tribol includes
 #include "tribol/config.hpp"
 
 #ifdef TRIBOL_USE_ENZYME
 /*
  * Enzyme is an LLVM-based automatic differentiation (AD) tool that enables
  * efficient computation of derivatives directly from code at compile time.
- * 
+ *
  * Variables prefixed with enzyme_* or function types prefixed with __enzyme_*,
  * are variables which will get preprocessed in the LLVM intermediate
  * representation when the Enzyme LLVM plugin is loaded.
