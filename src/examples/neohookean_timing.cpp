0f62d2cda11814e6a2fbc71ba9c669eaaaad6af7
Ryan Lutz <lutz23@llnl.gov>
2025-06-18 16:41:27 -0700

    cleaned up code/ added lambda functions to reduce

--- src/examples/neohookean_timing.cpp
+++ src/examples/neohookean_timing.cpp
@@ -2,12 +2,48 @@
 #include "../tests/neohookean_common.hpp"
 #include <cstdlib>
 #include <cstdio>
-#include <array>
 #include "axom/core.hpp"
 #include "axom/slic/interface/slic.hpp"
-#include <cmath>
-#include <limits>
 #include "tribol/common/Enzyme.hpp"
+
+template <typename TangentFn>
+void do_dWdE_timing( double* E, double mu, double lambda, int N, TangentFn&& tangent_fn)
+{
+  double S[9] = {0.0};
+  axom::utilities::Timer timer{ false };
+  tangent_fn(E, mu, lambda, S);
+
+
+  timer.start();
+  for(int i = 0; i < N; ++i) {
+      tangent_fn(E, mu, lambda, S);
+      // for(int j = 0; j < 9; ++j) {
+      //     Dw[j] += S[j];
+      // }
+  }
+  timer.stop();
+  std::cout << axom::fmt::format( "{:f}ms", timer.elapsedTimeInMilliSec() ) << std::endl;
+}
+
+template <typename TangentFn>
+void do_dSdE_timing( double* E, double mu, double lambda, int N, TangentFn&& tangent_fn)
+{
+  double S[81] = {0.0};
+  axom::utilities::Timer timer{ false };
+  tangent_fn(E, mu, lambda, S);
+
+
+  timer.start();
+  for(int i = 0; i < N; ++i) {
+      tangent_fn(E, mu, lambda, S);
+      // for(int j = 0; j < 9; ++j) {
+      //     Dw[j] += S[j];
+      // }
+  }
+  timer.stop();
+  std::cout << axom::fmt::format( "{:f}ms", timer.elapsedTimeInMilliSec() ) << std::endl;
+}
+
 
 int main()
 {
@@ -17,19 +53,51 @@ int main()
   int N = 0;
   std::cout << "Enter cycles: ";
   std::cin >> N;
-  double dw_dE[9] = { 0.0 };
-  double d2W_d2E[81] = { 0.0 };
-  double h = 1e-7;
+  std::cout << "Time to calc stress with fwddiff: ";
 
-  run_fwd_mode( E, mu, lambda, dw_dE, N );
-  run_bkwd_mode( E, mu, lambda, dw_dE, N );
-  run_hand_derivative( E, mu, lambda, dw_dE, N );
-  run_fwd_fwd( E, mu, lambda, d2W_d2E, N );
-  run_rev_fwd( E, mu, lambda, d2W_d2E, N );
-  run_fwd_rev( E, mu, lambda, d2W_d2E, N );
-  run_rev_rev( E, mu, lambda, d2W_d2E, N );
-  run_fwd_FD( E, mu, lambda, d2W_d2E, N, h );
-  run_hand_fwd( E, mu, lambda, d2W_d2E, N );
+  do_dWdE_timing(E, mu, lambda, N, [](double * E, double mu, double lambda, double* S) {
+    stress(E, mu, lambda, S);
+  });
+  
+  std::cout << "Time to calc stress with autodiff: ";
+  do_dWdE_timing(E, mu, lambda, N, [](double * E, double mu, double lambda, double* S) {
+    stress_reverse(E, mu, lambda, S);
+  });
+  
+  std::cout << "Time to calc stress with hand coded deriv: ";
+  do_dWdE_timing(E, mu, lambda, N, [](double * E, double mu, double lambda, double* S) {
+    hand_code_deriv(E, mu, lambda, S);
+  });
+  
+  std::cout << "Time to calc 2nd deriv with fwd_fwd: ";
+  do_dSdE_timing(E, mu, lambda, N, [](double * E, double mu, double lambda, double* S) {
+    second_deriv_fwd_fwd(E, mu, lambda, S);
+  });
+  
+  std::cout << "Time to calc 2nd deriv with rev_fwd: ";
+  do_dSdE_timing(E, mu, lambda, N, [](double * E, double mu, double lambda, double* S) {
+    second_deriv_rev_fwd(E, mu, lambda, S);
+  });
+  
+  std::cout << "Time to calc 2nd deriv with fwd_rev: ";
+  do_dSdE_timing(E, mu, lambda, N, [](double * E, double mu, double lambda, double* S) {
+    second_deriv_fwd_rev(E, mu, lambda, S);
+  });
+  
+  std::cout << "Time to calc 2nd deriv with rev_rev: ";
+  do_dSdE_timing(E, mu, lambda, N, [](double * E, double mu, double lambda, double* S) {
+    second_deriv_rev_rev(E, mu, lambda, S);
+  });
+  
+  std::cout << "Time to calc 2nd deriv with fwd_FD: ";
+  do_dSdE_timing(E, mu, lambda, N, [](double * E, double mu, double lambda, double* S) {
+    second_deriv_fwd_FD(E, mu, lambda, S);
+  });
+  
+  std::cout << "Time to calc 2nd deriv with hand_fwd: ";
+  do_dSdE_timing(E, mu, lambda, N, [](double * E, double mu, double lambda, double* S) {
+    second_deriv_hand_fwd(E, mu, lambda, S);
+  });
 
   return 0;
 }
--- src/tests/enzyme_neohookean.cpp
+++ src/tests/enzyme_neohookean.cpp
@@ -3,16 +3,14 @@
 #include "neohookean_common.hpp"
 #include <cstdlib>
 #include <cstdio>
-#include <array>
-#include <cmath>
-#include <limits>
-#include "tribol/common/Enzyme.hpp"
+
 
 class EnzymeNeohookeanTest : public testing::Test {
  protected:
+//clang-format off
   double F_all[27] = { 1.0, 0.5,  0.0,  0.0,  1.2,  0.1, 0.0,  0.0, 1.0, 1.1,  0.7,  0.1, 0.2, 1.4,
                        0.2, 0.02, 0.04, 1.05, 1.05, 0.2, 0.04, 0.0, 1.1, 0.14, 0.07, 0.0, 1.09 };
-
+//clang-format on
   double mu = 1.6;
   double lambda = 1.2;
 
@@ -28,95 +26,86 @@ class EnzymeNeohookeanTest : public testing::Test {
   }
 };
 
-TEST_F( EnzymeNeohookeanTest, finite_difference_vs_exact )
+
+template <typename StressFn>
+void test_stress( double* E_all, double mu, double lambda, double tol, StressFn&& stress_test_fn)
 {
   for ( int i = 0; i < 3; ++i ) {
     double S_FD[9] = { 0.0 };
-    double S_exact[9] = { 0.0 };
+    double S_test[9] = { 0.0 };
     stress_FD( E_all + i * 9, mu, lambda, S_FD );
-    hand_code_deriv( E_all + i * 9, mu, lambda, S_exact );
+    stress_test_fn( E_all + i * 9, mu, lambda, S_test );
     for ( int j = 0; j < 9; ++j ) {
-      EXPECT_NEAR( S_FD[j], S_exact[j], tol );
+      EXPECT_NEAR( S_FD[j], S_test[j], tol );
     }
   }
+}
+
+
+template <typename StressFn>
+void test_stress_2nd_deriv( double* E_all, double mu, double lambda, double tol, StressFn&& deriv2_test_fn) 
+{
+  for ( int i = 0; i < 3; ++i ) {
+    double dW2_FD[81] = { 0.0 };
+    double dW2_test[81] = { 0.0 };
+    second_deriv_fwd_FD( E_all + i * 9, mu, lambda, dW2_FD );
+    deriv2_test_fn( E_all + i * 9, mu, lambda, dW2_test );
+    for ( int j = 0; j < 81; ++j ) {
+      EXPECT_NEAR( dW2_FD[j], dW2_test[j], tol );
+    }
+  }
+}
+
+
+
+TEST_F( EnzymeNeohookeanTest, finite_difference_vs_exact )
+{
+  test_stress(E_all, mu, lambda, tol, [](double* E, double mu, double lambda, double* S) {
+    hand_code_deriv(E, mu, lambda, S);
+  });
 }
 
 TEST_F( EnzymeNeohookeanTest, finite_difference_vs_fwd_diff )
 {
-  for ( int i = 0; i < 3; ++i ) {
-    double S_FD[9] = { 0.0 };
-    double S_fwddiff[9] = { 0.0 };
-    stress_FD( E_all + i * 9, mu, lambda, S_FD );
-    stress( E_all + i * 9, mu, lambda, S_fwddiff );
-    for ( int j = 0; j < 9; ++j ) {
-      EXPECT_NEAR( S_FD[j], S_fwddiff[j], tol );
-    }
-  }
+  test_stress(E_all, mu, lambda, tol, [](double* E, double mu, double lambda, double* S) {
+    stress(E, mu, lambda, S);
+  });
 }
+
 
 TEST_F( EnzymeNeohookeanTest, finite_difference_vs_autodiff )
 {
-  for ( int i = 0; i < 3; ++i ) {
-    double S_FD[9] = { 0.0 };
-    double S_autodiff[9] = { 0.0 };
-    stress_FD( E_all + i * 9, mu, lambda, S_FD );
-    stress_reverse( E_all + i * 9, mu, lambda, S_autodiff );
-    for ( int j = 0; j < 9; ++j ) {
-      EXPECT_NEAR( S_FD[j], S_autodiff[j], tol );
-    }
-  }
+  test_stress(E_all, mu, lambda, tol, [](double* E, double mu, double lambda, double* S) {
+    stress_reverse(E, mu, lambda, S);
+  });
 }
 
 TEST_F( EnzymeNeohookeanTest, 2nd_deriv_finite_difference_vs_fwd_fwd )
 {
-  for ( int i = 0; i < 3; ++i ) {
-    double dW2_FD[81] = { 0.0 };
-    double dW2_fwd_fwd[81] = { 0.0 };
-    second_deriv_fwd_FD( E_all + i * 9, mu, lambda, dW2_FD );
-    second_deriv_fwd_fwd( E_all + i * 9, mu, lambda, dW2_fwd_fwd );
-    for ( int j = 0; j < 9; ++j ) {
-      EXPECT_NEAR( dW2_FD[j], dW2_fwd_fwd[j], tol );
-    }
-  }
+  test_stress_2nd_deriv(E_all, mu, lambda, tol, [](double* E, double mu, double lambda, double* S) {
+    second_deriv_fwd_fwd(E, mu, lambda, S);
+  });
 }
 
 TEST_F( EnzymeNeohookeanTest, 2nd_deriv_finite_difference_vs_fwd_rev )
-{
-  for ( int i = 0; i < 3; ++i ) {
-    double dW2_FD[81] = { 0.0 };
-    double dW2_fwd_rev[81] = { 0.0 };
-    second_deriv_fwd_FD( E_all + i * 9, mu, lambda, dW2_FD );
-    second_deriv_fwd_rev( E_all + i * 9, mu, lambda, dW2_fwd_rev );
-    for ( int j = 0; j < 81; ++j ) {
-      EXPECT_NEAR( dW2_FD[j], dW2_fwd_rev[j], tol );
-    }
-  }
+  {
+    test_stress_2nd_deriv(E_all, mu, lambda, tol, [](double* E, double mu, double lambda, double* S) {
+      second_deriv_fwd_rev(E, mu, lambda, S);
+    });
 }
 
 TEST_F( EnzymeNeohookeanTest, 2nd_deriv_finite_difference_vs_rev_rev )
-{
-  for ( int i = 0; i < 3; ++i ) {
-    double dW2_FD[81] = { 0.0 };
-    double dW2_rev_rev[81] = { 0.0 };
-    second_deriv_fwd_FD( E_all + i * 9, mu, lambda, dW2_FD );
-    second_deriv_rev_rev( E_all + i * 9, mu, lambda, dW2_rev_rev );
-    for ( int j = 0; j < 81; ++j ) {
-      EXPECT_NEAR( dW2_FD[j], dW2_rev_rev[j], tol );
-    }
-  }
+  {
+    test_stress_2nd_deriv(E_all, mu, lambda, tol, [](double* E, double mu, double lambda, double* S) {
+      second_deriv_rev_rev(E, mu, lambda, S);
+    });
 }
 
 TEST_F( EnzymeNeohookeanTest, 2nd_deriv_finite_difference_vs_rev_fwd )
-{
-  for ( int i = 0; i < 3; ++i ) {
-    double dW2_FD[81] = { 0.0 };
-    double dW2_rev_fwd[81] = { 0.0 };
-    second_deriv_fwd_FD( E_all + i * 9, mu, lambda, dW2_FD );
-    second_deriv_rev_rev( E_all + i * 9, mu, lambda, dW2_rev_fwd );
-    for ( int j = 0; j < 81; ++j ) {
-      EXPECT_NEAR( dW2_FD[j], dW2_rev_fwd[j], tol );
-    }
-  }
+  {
+    test_stress_2nd_deriv(E_all, mu, lambda, tol, [](double* E, double mu, double lambda, double* S) {
+      second_deriv_rev_fwd(E, mu, lambda, S);
+    });
 }
 
 TEST_F( EnzymeNeohookeanTest, 2nd_deriv_finite_difference_vs_hand_fwd )
