dcb4b8dedd760f571896875d9eff49c56f99525c
Eric B. Chin <chin23@llnl.gov>
2025-11-12 09:20:27 -0800

    just copy/paste mfem header

--- src/tribol/common/Enzyme.hpp
+++ src/tribol/common/Enzyme.hpp
@@ -9,37 +9,46 @@
 // Tribol config include
 #include "tribol/config.hpp"
 
-#include "mfem.hpp"
-
-#if defined( MFEM_USE_ENZYME )
-
-#include "mfem/general/enzyme.hpp"
-
-#elif defined( TRIBOL_USE_ENZYME )
-/*
- * Enzyme is an LLVM-based automatic differentiation (AD) tool that enables efficient computation of derivatives
- * directly from code at compile time.
- *
- * Variables prefixed with enzyme_* or function types prefixed with __enzyme_*, are variables which will get
- * preprocessed in the LLVM intermediate representation when the Enzyme LLVM plugin is loaded.
- *
- * For more details, see the Enzyme documentation: https://enzyme.mit.edu
- */
+// Tribol includes
+#include "tribol/common/BasicTypes.hpp"
 
 extern int enzyme_dup;
 extern int enzyme_dupnoneed;
 extern int enzyme_out;
 extern int enzyme_const;
+extern int enzyme_interleave;
 
-extern int enzyme_runtime_activity;
-
-// Reverse mode autodiff
-template <typename return_type, typename... Args>
-return_type __enzyme_autodiff( Args... );
-
-// Forward mode autodiff
-template <typename return_type, typename... Args>
-return_type __enzyme_fwddiff( Args... );
+#if defined( TRIBOL_USE_CUDA ) || defined( TRIBOL_USE_HIP )
+#define TRIBOL_DEVICE_EXTERN_STMT( name ) extern __device__ int name;
+#else
+#define TRIBOL_DEVICE_EXTERN_STMT( name )
 #endif
+
+TRIBOL_DEVICE_EXTERN_STMT( enzyme_dup )
+TRIBOL_DEVICE_EXTERN_STMT( enzyme_dupnoneed )
+TRIBOL_DEVICE_EXTERN_STMT( enzyme_out )
+TRIBOL_DEVICE_EXTERN_STMT( enzyme_const )
+TRIBOL_DEVICE_EXTERN_STMT( enzyme_interleave )
+
+// warning: if inlined, triggers function '__enzyme_autodiff' is not defined
+template <typename return_type, typename... Args>
+TRIBOL_HOST_DEVICE return_type __enzyme_autodiff( Args... );
+
+// warning: if inlined, triggers function '__enzyme_fwddiff' is not defined
+template <typename return_type, typename... Args>
+TRIBOL_HOST_DEVICE return_type __enzyme_fwddiff( Args... );
+
+#define TRIBOL_ENZYME_INACTIVENOFREE __attribute__( ( enzyme_inactive, enzyme_nofree ) )
+#define TRIBOL_ENZYME_INACTIVE __attribute__( ( enzyme_inactive ) )
+#define TRIBOL_ENZYME_FN_LIKE( x ) __attribute__( ( enzyme_function_like( #x ) ) )
+
+#else
+#define TRIBOL_ENZYME_INACTIVENOFREE
+#define TRIBOL_ENZYME_INACTIVE
+#define TRIBOL_ENZYME_FN_LIKE( x )
+#endif
+
+#define TRIBOL_ENZYME_FN_LIKE_FREE TRIBOL_ENZYME_FN_LIKE( free )
+#define TRIBOL_ENZYME_FN_LIKE_DYNCAST TRIBOL_ENZYME_FN_LIKE( __dynamic_cast )
 
 #endif /* TRIBOL_COMMON_ENZYME_HPP_ */
