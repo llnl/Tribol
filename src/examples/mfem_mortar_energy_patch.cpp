136b8c4bf2aebf812340d77c4415e5892c6c012c
Eric B. Chin <chin23@llnl.gov>
2026-02-05 22:57:14 -0800

    get rid of mpi wait for debugging

--- src/examples/mfem_mortar_energy_patch.cpp
+++ src/examples/mfem_mortar_energy_patch.cpp
@@ -60,19 +60,6 @@ int main( int argc, char** argv )
   int rank;
   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 
-  // // Only make Rank 0 wait (or whichever rank you want to debug)
-  // if ( rank == 0 ) {
-  //   volatile int debug_wait = 1;
-  //   printf( "Rank %d is ready to attach. PID: %d\n", rank, getpid() );
-  //   fflush( stdout );
-
-  //   while ( debug_wait ) {
-  //     sleep( 1 );  // Sleep to avoid burning 100% CPU
-  //   }
-  // }
-
-  // MPI_Barrier( MPI_COMM_WORLD );  // Keep other ranks from running ahead
-
 #ifdef TRIBOL_USE_UMPIRE
   umpire::ResourceManager::getInstance();  // initialize umpire's ResouceManager
 #endif
