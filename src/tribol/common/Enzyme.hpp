4f108a5931a099174f7efc8232f2a4736800f255
Eric B. Chin <chin23@llnl.gov>
2026-02-13 09:28:47 -0800

    backup defs when mfem is built without enzyme

--- src/tribol/common/Enzyme.hpp
+++ src/tribol/common/Enzyme.hpp
@@ -11,7 +11,49 @@
 #include "tribol/common/BasicTypes.hpp"
 
 #ifdef TRIBOL_USE_ENZYME
+#ifdef MFEM_USE_ENZYME
 #include "mfem/general/enzyme.hpp"
+#else  // MFEM_USE_ENZYME
+// NOTE: contents copied from MFEM's general/enzyme.hpp
+
+/*
+ * Variables prefixed with enzyme_* or function types prefixed with __enzyme_*,
+ * are variables which will get preprocessed in the LLVM intermediate
+ * representation when the Enzyme LLVM plugin is loaded. See the Enzyme
+ * documentation (https://enzyme.mit.edu) for more information.
+ */
+
+extern int enzyme_dup;
+extern int enzyme_dupnoneed;
+extern int enzyme_out;
+extern int enzyme_const;
+extern int enzyme_interleave;
+
+#if defined( MFEM_USE_CUDA ) || defined( MFEM_USE_HIP )
+#define MFEM_DEVICE_EXTERN_STMT( name ) extern __device__ int name;
+#else
+#define MFEM_DEVICE_EXTERN_STMT( name )
+#endif
+
+MFEM_DEVICE_EXTERN_STMT( enzyme_dup )
+MFEM_DEVICE_EXTERN_STMT( enzyme_dupnoneed )
+MFEM_DEVICE_EXTERN_STMT( enzyme_out )
+MFEM_DEVICE_EXTERN_STMT( enzyme_const )
+MFEM_DEVICE_EXTERN_STMT( enzyme_interleave )
+
+// warning: if inlined, triggers function '__enzyme_autodiff' is not defined
+template <typename return_type, typename... Args>
+MFEM_HOST_DEVICE return_type __enzyme_autodiff( Args... );
+
+// warning: if inlined, triggers function '__enzyme_fwddiff' is not defined
+template <typename return_type, typename... Args>
+MFEM_HOST_DEVICE return_type __enzyme_fwddiff( Args... );
+
+#define MFEM_ENZYME_INACTIVENOFREE __attribute__( ( enzyme_inactive, enzyme_nofree ) )
+#define MFEM_ENZYME_INACTIVE __attribute__( ( enzyme_inactive ) )
+#define MFEM_ENZYME_FN_LIKE( x ) __attribute__( ( enzyme_function_like( #x ) ) )
+
+#endif  // MFEM_USE_ENZYME
 
 #if !defined( TRIBOL_USE_HOST ) && !defined( TRIBOL_DEVICE_CODE )
 // When compiling with NVCC or HIPCC, the compiler performs multiple passes.
@@ -33,7 +75,7 @@ extern int tribol_host_enzyme_dupnoneed asm( "enzyme_dupnoneed" );
 #define TRIBOL_ENZYME_OUT tribol_host_enzyme_out
 #define TRIBOL_ENZYME_DUPNONEED tribol_host_enzyme_dupnoneed
 
-#else
+#else  // !defined( TRIBOL_USE_HOST ) && !defined( TRIBOL_DEVICE_CODE )
 // We are either:
 // 1. In a device compilation pass (__CUDA_ARCH__ or __HIP_DEVICE_COMPILE__ defined).
 // 2. Using a standard host compiler (GCC, Clang, etc.).
@@ -42,14 +84,14 @@ extern int tribol_host_enzyme_dupnoneed asm( "enzyme_dupnoneed" );
 #define TRIBOL_ENZYME_DUP enzyme_dup
 #define TRIBOL_ENZYME_OUT enzyme_out
 #define TRIBOL_ENZYME_DUPNONEED enzyme_dupnoneed
-#endif
+#endif  // !defined( TRIBOL_USE_HOST ) && !defined( TRIBOL_DEVICE_CODE )
 
-#else
+#else  // TRIBOL_USE_ENZYME
 // Fallback definitions if Enzyme is disabled
 #define TRIBOL_ENZYME_CONST 0
 #define TRIBOL_ENZYME_DUP 0
 #define TRIBOL_ENZYME_OUT 0
 #define TRIBOL_ENZYME_DUPNONEED 0
-#endif
+#endif  // TRIBOL_USE_ENZYME
 
 #endif /* SRC_TRIBOL_COMMON_ENZYME_HPP_ */
