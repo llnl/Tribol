8f9d476cd5fb29b7701bb9c8428857c3e929f798
Eric B. Chin <chin23@llnl.gov>
2026-02-13 23:09:01 -0800

    fix formatting and includes

--- src/tests/tribol_mfem_jacobian.cpp
+++ src/tests/tribol_mfem_jacobian.cpp
@@ -3,22 +3,25 @@
 //
 // SPDX-License-Identifier: (MIT)
 
-#include <gtest/gtest.h>
-#include "mfem.hpp"
-#include "axom/slic.hpp"
-#include "shared/mesh/MeshBuilder.hpp"
 #include "tribol/config.hpp"
-#include "tribol/common/Parameters.hpp"
-#include "tribol/interface/tribol.hpp"
-#include "tribol/interface/mfem_tribol.hpp"
-#include "tribol/mesh/CouplingScheme.hpp"
-#include "tribol/mesh/MfemData.hpp"
+
+#include <gtest/gtest.h>
 
 #ifdef TRIBOL_USE_UMPIRE
 #include "umpire/ResourceManager.hpp"
 #endif
 
+#include "mfem.hpp"
+
+#include "axom/slic.hpp"
+
+#include "shared/mesh/MeshBuilder.hpp"
+
+#include "tribol/common/Parameters.hpp"
+#include "tribol/interface/tribol.hpp"
+#include "tribol/interface/mfem_tribol.hpp"
 #include "tribol/mesh/CouplingScheme.hpp"
+#include "tribol/mesh/MfemData.hpp"
 
 class MfemJacobianTest : public testing::Test {
  protected:
@@ -34,7 +37,7 @@ TEST_F( MfemJacobianTest, direct_jacobian_assembly )
   // Setup simple two-cube mesh
   int ref_levels = 0;
   int nel_per_dir = std::pow( 2, ref_levels );
-  
+
   auto mortar_attrs = std::set<int>( { 4 } );
   auto nonmortar_attrs = std::set<int>( { 5 } );
 
@@ -67,10 +70,9 @@ TEST_F( MfemJacobianTest, direct_jacobian_assembly )
   int cs_id = 0;
   int mesh1_id = 0;
   int mesh2_id = 1;
-  tribol::registerMfemCouplingScheme( cs_id, mesh1_id, mesh2_id, mesh, coords, mortar_attrs,
-                                      nonmortar_attrs, tribol::SURFACE_TO_SURFACE, tribol::NO_SLIDING,
-                                      tribol::SINGLE_MORTAR, tribol::FRICTIONLESS, tribol::LAGRANGE_MULTIPLIER,
-                                      tribol::BINNING_GRID );
+  tribol::registerMfemCouplingScheme( cs_id, mesh1_id, mesh2_id, mesh, coords, mortar_attrs, nonmortar_attrs,
+                                      tribol::SURFACE_TO_SURFACE, tribol::NO_SLIDING, tribol::SINGLE_MORTAR,
+                                      tribol::FRICTIONLESS, tribol::LAGRANGE_MULTIPLIER, tribol::BINNING_GRID );
 
   // Build internal MfemData
   tribol::updateMfemParallelDecomposition();
@@ -86,9 +88,9 @@ TEST_F( MfemJacobianTest, direct_jacobian_assembly )
     auto* submesh_data = cs->getMfemSubmeshData();
     ASSERT_NE( mesh_data, nullptr );
     ASSERT_NE( submesh_data, nullptr );
-    
-    cs->setMfemJacobianData( std::make_unique<tribol::MfemJacobianData>( 
-        *mesh_data, *submesh_data, cs->getContactMethod() ) );
+
+    cs->setMfemJacobianData(
+        std::make_unique<tribol::MfemJacobianData>( *mesh_data, *submesh_data, cs->getContactMethod() ) );
     jac_data = cs->getMfemJacobianData();
   }
   ASSERT_NE( jac_data, nullptr );
@@ -97,26 +99,26 @@ TEST_F( MfemJacobianTest, direct_jacobian_assembly )
 
   // Synthesize ComputedElementData for mortar-mortar block
   std::vector<tribol::ComputedElementData> contributions;
-  
-  int num_dofs_per_elem = 8 * dim; // Hex element, 8 nodes
+
+  int num_dofs_per_elem = 8 * dim;  // Hex element, 8 nodes
   int mat_size = num_dofs_per_elem * num_dofs_per_elem;
-  
+
   tribol::ComputedElementData contrib;
   contrib.row_space = tribol::BlockSpace::MORTAR;
   contrib.col_space = tribol::BlockSpace::MORTAR;
-  
+
   auto* mesh_data = cs->getMfemMeshData();
-  
-  if (mesh_data && mesh_data->GetMesh1NE() > 0) {
-      contrib.row_elem_ids.push_back(0);
-      contrib.col_elem_ids.push_back(0);
-      
-      contrib.jacobian_data.resize(mat_size);
-      for(int i=0; i<mat_size; ++i) contrib.jacobian_data[i] = 1.0;
-      
-      contrib.jacobian_offsets.push_back(0);
-      
-      contributions.push_back(contrib);
+
+  if ( mesh_data && mesh_data->GetMesh1NE() > 0 ) {
+    contrib.row_elem_ids.push_back( 0 );
+    contrib.col_elem_ids.push_back( 0 );
+
+    contrib.jacobian_data.resize( mat_size );
+    for ( int i = 0; i < mat_size; ++i ) contrib.jacobian_data[i] = 1.0;
+
+    contrib.jacobian_offsets.push_back( 0 );
+
+    contributions.push_back( contrib );
   }
 
   // Assemble Jacobian
@@ -125,14 +127,14 @@ TEST_F( MfemJacobianTest, direct_jacobian_assembly )
   // Verify non-zeros if contributions were added
   int local_contrib = contributions.size();
   int global_contrib = 0;
-  MPI_Allreduce(&local_contrib, &global_contrib, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-  
-  if (global_contrib > 0) {
-      EXPECT_GT( ParJ->NNZ(), 0 );
+  MPI_Allreduce( &local_contrib, &global_contrib, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
+
+  if ( global_contrib > 0 ) {
+    EXPECT_GT( ParJ->NNZ(), 0 );
   } else {
-      if (n_ranks == 1) {
-          FAIL() << "No surface elements found on mesh 1 in serial run.";
-      }
+    if ( n_ranks == 1 ) {
+      FAIL() << "No surface elements found on mesh 1 in serial run.";
+    }
   }
 }
 
