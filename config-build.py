0a40f0c587a67b1463c7690a40e66703758218d7
chapman39 <chapman39@llnl.gov>
2026-08-04 13:31:47 -0700

    update configure script to handle CMAKE_BUILD_TYPE as an arg

--- config-build.py
+++ config-build.py
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2017-2025, Lawrence Livermore National Security, LLC and
+# Copyright (c) 2017-2026, Lawrence Livermore National Security, LLC and
 # other Tribol Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: (MIT)
@@ -31,6 +31,18 @@ def get_machine_name():
     return socket.gethostname().rstrip('1234567890')
 
 def get_default_host_config():
+    # Prefer the repo helper script when present. This allows SYS_TYPE-aware
+    # selection and compiler preference (e.g., prefer LLVM unless specified).
+    repodir = os.path.dirname(os.path.abspath(__file__))
+    helper = os.path.join(repodir, "skills", "building", "scripts", "determine_host_config")
+    if os.path.isfile(helper) and os.access(helper, os.X_OK):
+        try:
+            hostconfig = subprocess.check_output([helper, "--basename"], text=True).strip()
+            if hostconfig:
+                return hostconfig
+        except Exception:
+            pass
+
     machine_name = get_machine_name()
 
     if machine_name in _host_configs_map.keys():
@@ -73,8 +85,7 @@ def parse_arguments():
                         "--buildtype",
                         type=str,
                         choices=["Release", "Debug", "RelWithDebInfo", "MinSizeRel"],
-                        default="Debug",
-                        help="build type.")
+                        help="build type. defaults to Release")
 
     parser.add_argument("-e",
                         "--eclipse",
@@ -100,8 +111,11 @@ def parse_arguments():
                         action='store_true',
                         help="print the machine name for this system and exit")
 
+    parser.add_argument("-n", 
+                        "--ninja",
+                        action='store_true',
+                        help="use ninja generator to build Smith instead of make")
 
-    
     args, unknown_args = parser.parse_known_args()
     if unknown_args:
         print("[config-build]: Passing the following arguments directly to cmake... %s" % unknown_args)
@@ -225,6 +239,7 @@ def create_cmake_command_line(args, unknown_args, buildpath, installpath, hostco
 
     # Add build type (opt or debug)
     cmakeline += " -DCMAKE_BUILD_TYPE=" + args.buildtype
+
     # Set install dir
     cmakeline += " -DCMAKE_INSTALL_PREFIX=%s" % installpath
 
@@ -233,6 +248,9 @@ def create_cmake_command_line(args, unknown_args, buildpath, installpath, hostco
 
     if args.eclipse:
         cmakeline += ' -G "Eclipse CDT4 - Unix Makefiles"'
+
+    if args.ninja:
+        cmakeline += ' -G Ninja'
 
     if unknown_args:
         cmakeline += " " + " ".join( unknown_args )
@@ -269,7 +287,7 @@ def run_cmake(buildpath, cmakeline):
 # Main
 ############################
 def main():
-    repodir = os.path.abspath(os.path.dirname(__file__))     
+    repodir = os.path.abspath(os.path.dirname(__file__))
     assert os.path.abspath(os.getcwd())==repodir, "config-build must be run from %s" % repodir
 
     args, unknown_args = parse_arguments()
@@ -286,6 +304,15 @@ def main():
           return True
        else:
           return False
+
+    if args.buildtype == None:
+        # Set default CMake build type
+        args.buildtype = "Release"
+        # If CMAKE_BUILD_TYPE was passed in as an argument, use that option instead
+        for unknown_arg in unknown_args:
+            if "-DCMAKE_BUILD_TYPE" in unknown_arg:
+                args.buildtype = unknown_arg.split("=")[1]
+                break
 
     basehostconfigpath = find_host_config(args, repodir)
     platform_info = get_platform_info(basehostconfigpath)
